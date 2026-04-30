f7fc6d50f7f72b06fdbb1b1ae1479aa04320c9b7
janinjaakohaha <jaineskajynsn@gmail.com>
2026-04-30 21:27:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    host: '0.0.0.0', // Critical for Render
+    port: 10000,      // Render's default port
+    strictPort: true,
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    strictPort: true,
+  }
 })
