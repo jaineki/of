529c2dc2bd6749d27cdedddeb09c31061076f90f
janinjaakohaha <jaineskajynsn@gmail.com>
2026-04-30 20:12:53 +0800

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+// src/types/index.ts
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  tags: string[];
+  demoUrl: string;
+  codeUrl: string;
+}
+
+export interface Skill {
+  name: string;
+  level: number; // 1-100
+  icon: string;
+}
+
+export interface SocialLink {
+  name: string;
+  url: string;
+  icon: string;
+}
